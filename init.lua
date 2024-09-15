999fbb25a691e8f92824d5254fcf3e49e11378a9
Luke Elrod <luke.elrod.career@gmail.com>
2024-09-15 02:15:40 -0700

    publish

---
+++ init.lua
@@ -0,0 +1,27 @@
+require("config.lazy")
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.wo.number = true
+
+-- Move line up (n)
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+-- Move line down (n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+-- Move line up (v)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+-- Move line down (v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+
+--telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+--colorscheme
+vim.cmd.colorscheme("rose-pine-moon")
+
+--lsps that arent in mason
+require'lspconfig'.dartls.setup{}
+
