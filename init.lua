e9b53d983f5d9ff4cceb1ddeeb264bab0464ab3f
Luke Elrod <luke.elrod.career@gmail.com>
2024-09-23 02:21:52 -0700

    remap ex

--- init.lua
+++ init.lua
@@ -16,6 +16,8 @@ vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 -- esc to exit terminal insert mode
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]]);
+--netrc
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 --telescope
 local builtin = require('telescope.builtin')
