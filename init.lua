dac0b6ff96319936b18448f611a774b0959477a6
Luke Elrod <luke.elrod.career@gmail.com>
2025-08-21 11:40:45 -0700

    feat: switch to oil

--- init.lua
+++ init.lua
@@ -1,20 +1,5 @@
 require("config.lazy")
 
-local function nvim_tree_attach(bufnr)
-    local api = require "nvim-tree.api"
-
-    local function opts(desc)
-        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
-
-    -- default mappings
-    api.config.mappings.default_on_attach(bufnr)
-
-    -- custom mappings
-    vim.keymap.set('n', '<BS>', api.tree.close, opts('Close'))
-end
-
-
 local function close_window()
     local state = require('barbar.state')
     if #state.buffers > 1 then
@@ -25,18 +10,16 @@ end
 --disable netrrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-require("nvim-tree").setup({
-    on_attach = nvim_tree_attach,
-    filters = {
-        enable = false
-    },
-})
 
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.wo.relativenumber = true
+
+vim.keymap.set("n", "<Tab>", function ()
+   require("oil").open()
+end)
 
 -- Move line up in normal mode
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { silent = true })
@@ -47,9 +30,6 @@ vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { silent = true })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
 -- Move selection down in visual mode
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
---nvim-tree
-vim.keymap.set("n", "<Tab>", ":NvimTreeFocus<CR>")
-vim.keymap.set("n", "<S-Tab>", ":NvimTreeFindFile<CR>")
 
 --unhighlight
 vim.keymap.set("n", "<C-h>", ":noh<CR>", { silent = true })
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,6 @@
+return {
+    'stevearc/oil.nvim',
+    main = "oil",
+    opts = {},
+    lazy = false,
+}
