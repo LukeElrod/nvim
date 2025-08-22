73fb7349600d29b0009b79712328f38a3b3c5884
Luke Elrod <luke.elrod.career@gmail.com>
2025-08-22 12:31:31 -0700

    fix: treesitter compilers

--- lua/treesitter/treesitter_setup.lua
+++ lua/treesitter/treesitter_setup.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M:setup()
     local treesitter = require('nvim-treesitter')
+    require 'nvim-treesitter.install'.compilers = { "clang", "gcc", "cc", "cl" }
     treesitter.install { "c", "cpp", "c_sharp", "java", "javascript", "dart", "python", "html", "css", "kotlin", "bash", "cmake", "make", "php", "lua", "rust", "json", "go", "markdown", "csv", "diff", "dockerfile", "gitignore", "typescript", "yaml", "groovy" }
 
     vim.api.nvim_create_autocmd('FileType', {
