0acc2a15e951c519f51eb853f9fb30532f61628d
Luke Elrod <luke.elrod.career@gmail.com>
2025-08-27 18:06:25 -0700

    lualine tweaks

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,13 @@ return {
     opts = {
         options = {
             theme = 'gruvbox-material'
+        },
+        sections = {
+            lualine_c = { { 'filename', path = 1 } },
+            lualine_y = { { 'lsp_status', ignore_lsp = { 'GitHub Copilot' } } }
+        },
+        inactive_sections = {
+            lualine_c = { { 'filename', path = 1 } }
         }
     }
 }
