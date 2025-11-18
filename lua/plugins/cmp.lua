935ff10164c18ba36bf62ce13f53ea5893f0d1c1
Luke Elrod <luke.elrod.career@gmail.com>
2025-11-18 13:13:22 -0800

    remove redundant setup

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,11 +1,4 @@
 return {
     { 'hrsh7th/nvim-cmp' },
-    {
-        'hrsh7th/cmp-nvim-lsp',
-        opts = {
-            sources = {
-                { name = 'nvim_lua' }
-            }
-        }
-    },
+    { 'hrsh7th/cmp-nvim-lsp' },
 }
