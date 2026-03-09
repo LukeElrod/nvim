440e14e10eb71e462f22e9688ac5b0b554095008
Luke Elrod <luke.elrod.career@gmail.com>
2026-03-09 14:04:20 -0700

    update: remove blink auto_insert

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -15,7 +15,15 @@ return {
             nerd_font_variant = 'mono'
         },
 
-        completion = { documentation = { auto_show = true, auto_show_delay_ms = 500, } },
+        completion = {
+            documentation = { auto_show = true, auto_show_delay_ms = 500, },
+            list = {
+                selection = {
+                    preselect = true,
+                    auto_insert = false
+                }
+            }
+        },
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
