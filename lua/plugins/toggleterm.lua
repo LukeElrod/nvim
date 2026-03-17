2829ff55fedf3fc0e2bccdaa9afa289a06f4ddb5
Luke Elrod <luke.elrod.career@gmail.com>
2026-03-16 17:27:12 -0700

    update: toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,4 @@
 return {'akinsho/toggleterm.nvim', version = "*", opts = {
     open_mapping = [[<C-`>]],
-    direction = 'float',
-    float_opts = {
-        border = 'curved',
-    },
+    size = 20
 }}
