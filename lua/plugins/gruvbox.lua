f3ef9b8c06e36a6b0868aed32d535fad264576b7
Luke Elrod <luke.elrod.career@gmail.com>
2026-04-13 18:27:20 -0700

    update: plugins, soft background

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -4,6 +4,7 @@ return {
 	priority = 1000,
 	opts = function()
 		vim.g.gruvbox_material_enable_italic = true
+		vim.g.gruvbox_material_background = "soft"
 		vim.cmd.colorscheme("gruvbox-material")
 	end,
 }
