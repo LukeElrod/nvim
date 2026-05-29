7e7fdd0af7512b418e7817dede1e0218aab5e9cc
Luke Elrod <luke.elrod.career@gmail.com>
2026-05-29 11:03:58 -0700

    fix: copilot chat necessary parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
 	"romus204/tree-sitter-manager.nvim",
 	opts = {
+		--to ensure copilot chat stuff renders right
+		ensure_installed = { "markdown", "markdown_inline", "html", "latex", "yaml" },
 		auto_install = true,
 	},
 }
