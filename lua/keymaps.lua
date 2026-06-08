109c85f9f9e470c5c2fbcf652460f1309879fde4
Luke Elrod <luke.elrod.career@gmail.com>
2026-06-08 16:38:12 -0700

    update: esc keymap, plugins

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,8 @@ vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { silent = true })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
 
+vim.keymap.set({ "i", "v" }, "<C-i>", "<Esc>")
+
 --unhighlight
 vim.keymap.set("n", "<leader>h", ":noh<CR>", { silent = true })
 
