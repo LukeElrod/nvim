ea32f1c85d62fa39a3b95d4f77dbbc681b50b3b6
Luke Elrod <luke.elrod.career@gmail.com>
2026-06-17 18:22:53 -0700

    feat: switch to lazygit
    much cleaner workflow

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,13 +1,20 @@
 return {
-	"NeogitOrg/neogit",
+	"kdheepak/lazygit.nvim",
 	lazy = true,
-	dependencies = {
-		"sindrets/diffview.nvim",
-		"m00qek/baleia.nvim",
-		"nvim-telescope/telescope.nvim",
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
 	},
-	cmd = "Neogit",
+	-- optional for floating window border decoration
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	-- setting the keybinding for LazyGit with 'keys' is recommended in
+	-- order to load the plugin when the command is run for the first time
 	keys = {
-		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 	},
 }
